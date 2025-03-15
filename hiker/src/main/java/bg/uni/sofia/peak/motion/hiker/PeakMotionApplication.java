4086ad2d74ba4c93c01ae2155e5b5d70c9e53035
Martin Nikolov <martivdev38@gmail.com>
2025-03-15 10:18:26 +0200

    Create branch and push java project

---
+++ hiker/src/main/java/bg/uni/sofia/peak/motion/hiker/PeakMotionApplication.java
@@ -0,0 +1,13 @@
+package bg.uni.sofia.peak.motion.hiker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeakMotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeakMotionApplication.class, args);
+	}
+
+}
---
+++ hiker/src/test/java/bg/uni/sofia/peak/motion/hiker/PeakMotionApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.uni.sofia.peak.motion.hiker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeakMotionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
